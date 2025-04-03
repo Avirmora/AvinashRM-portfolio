58f9d6d82950e354ed0f1538478e93baf834fa7b
Avinash Reddy Mora <avirmora@gmail.com>
2025-04-02 20:56:30 -0500

    added new commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>I am a passionate software developer with expertise in modern web technologies.</p>
+      </div>
+    );
+  }
+  
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <nav>
+          <a href="/about">About</a>
+          <a href="/projects">Projects</a>
+          <a href="/contact">Contact</a>
+        </nav>
+        <h2>Hello, I'm [Your Name]</h2>
+        <p>Software Developer | Tech Enthusiast</p>
+      </div>
+    );
+  }
